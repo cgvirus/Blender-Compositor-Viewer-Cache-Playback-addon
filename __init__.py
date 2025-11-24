835b6d1ddb1b11f0813dc124e106c6c215e4675e
Fahad Hasan Pathik <contact.fahadpathik@gmail.com>
2025-11-24 06:57:13 +0600

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,47 +1,38 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-
-bl_info = {
-    "name": "Compositor Viewer Cache",
-    "description": "Creates Disc Cache and playback in Viewer",
-    "author": "Fahad Hasan Pathik CGVIRUS",
-    "version": (1, 0),
-    "blender": (3, 0, 1),
-    "category": "Image Editor",
-    "doc_url": "https://github.com/cgvirus/Blender-Compositor-Viewer-Cache-Playback-addon",
-    }
 
 import bpy
-from . import Blender_Compositor_Viewer_Cache_Playback_Addon
+from . import Compositor_Scrub_Render, Compositor_Viewer_Cache_Playback
 
 
-modules = Blender_Compositor_Viewer_Cache_Playback_Addon
+modules = [Compositor_Scrub_Render, Compositor_Viewer_Cache_Playback]
 
 
 def register():
     from bpy.utils import register_class
-    modules.register()
+
+    for module in modules:
+        module.register()
+
 
 
 def unregister():
     from bpy.utils import unregister_class
-    modules.unregister()
+
+    for module in modules:
+        module.unregister()
+
 
 
 if __name__ == "__main__":
